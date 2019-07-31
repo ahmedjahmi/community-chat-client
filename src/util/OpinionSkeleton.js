22f1c7807fedf298c66019c5c07ba7d3e4b166d8
ahmedJahmi <amjahmi@gmail.com>
2019-07-30 23:17:13 -0400

    Add OpinionSkeleton component

--- src/util/OpinionSkeleton.js
+++ src/util/OpinionSkeleton.js
@@ -9,10 +9,48 @@ import CardContent from '@material-ui/core/CardContent';
 
 import withStyles from '@material-ui/core/styles/withStyles';
 
-const styles = {};
+const styles = {
+	card: {
+		display: 'flex',
+		marginBottom: 20
+	},
+	cardContent: {
+		width: '100%',
+		flexDirection: 'column',
+		padding: 25
+	},
+	cover: {
+		minWidth: 200,
+		objectFit: 'cover'
+	},
+	handle: {
+		width: 60,
+		height: 18,
+		backgroundColor: 'rgba(3, 169, 244, 1)',
+		marginBottom: 7
+	},
+	date: {
+		height: 14,
+		width: 100,
+		backgroundColor: 'rgba(0,0,0, 0.3)',
+		marginBottom: 10
+	},
+	fullLine: {
+		height: 15,
+		width: '300%',
+		backgroundColor: 'rgba(0,0,0, 0.3)',
+		marginBottom: 10
+	},
+	halfLine: {
+		height: 15,
+		width: '100%',
+		backgroundColor: 'rgba(0,0,0, 0.3)',
+		marginBottom: 10
+	}
+};
 
 const OpinionSkeleton = (props) => {
-    const {classes} = this.props;
+    const {classes} = props;
 
     const content = Array.from({ length: 5 }).map((item, index) => (
 			<Card className={classes.card} key={index}>
